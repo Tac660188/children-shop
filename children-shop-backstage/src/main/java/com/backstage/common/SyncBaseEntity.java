78e084437a88122dd0c5a18cb382ac6560b05429
yeqt <879049936@qq.com>
2021-11-01 17:42:34 +0800

    develop: 自动建表功能

--- children-shop-backstage/src/main/java/com/backstage/common/SyncBaseEntity.java
+++ children-shop-backstage/src/main/java/com/backstage/common/SyncBaseEntity.java
@@ -2,6 +2,7 @@ package com.backstage.common;
 
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
@@ -11,6 +12,7 @@ import java.util.Date;
  * @author hongdongqu
  */
 @MappedSuperclass
+@Data
 public class SyncBaseEntity extends BaseEntity {
     private static final long serialVersionUID = 1L;
 
@@ -28,35 +30,4 @@ public class SyncBaseEntity extends BaseEntity {
     @Column(name="his",  columnDefinition = "tinyint(1) COMMENT '是否为历史' default 0")
     private Boolean his;
 
-    public String getWorkId() {
-        return workId;
-    }
-
-    public void setWorkId(String workId) {
-        this.workId = workId;
-    }
-
-    public String getWorkName() {
-        return workName;
-    }
-
-    public void setWorkName(String workName) {
-        this.workName = workName;
-    }
-
-    public Date getSyncTime() {
-        return syncTime;
-    }
-
-    public void setSyncTime(Date syncTime) {
-        this.syncTime = syncTime;
-    }
-
-    public Boolean getHis() {
-        return his;
-    }
-
-    public void setHis(Boolean his) {
-        this.his = his;
-    }
 }
